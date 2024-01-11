ab9afd2c4ace68055ad74dc955208543d6ec1413
Muhammad Ramzan <techsramzan@gmail.com>
2024-01-11 21:15:19 +0500

    Connected Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/auth/login_screen.dart';
 
 late Size mq;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initializeFireBase();
   runApp(const MyApp());
 }
 
@@ -32,4 +35,10 @@ class MyApp extends StatelessWidget {
       home: LoginScreen(),
     );
   }
+}
+
+_initializeFireBase() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
